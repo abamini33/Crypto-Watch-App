bbacb2dd6882ce8409889d8fed1c4d3d888c56ca
abamini33 <pierrealex33200@gmail.com>
2024-03-08 17:52:57 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/index.scss";
+
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
